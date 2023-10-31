90109c92d4a6bdc04ee3cb6ef0a17e748bc906a4
grantgabriel <grantgabriel30@gmail.com>
2023-10-31 21:19:57 +0700

    First commit agah

---
+++ Basics Fundamental/structures.cpp
@@ -0,0 +1,13 @@
+// Filename : structures.cpp
+
+#include <bits/stdc++.h>             // Complete set of C++ library
+using namespace std;                 // Namespaces to 
+                                     // prevents variables name error
+
+int main() {
+    cout << "Hello World!" << endl;  // cout to print into the std output
+                                     // endl to make a new line
+
+    return 0;                        // exit condition, 0 = exit_success
+                                     // 1 = exit_failure
+}
