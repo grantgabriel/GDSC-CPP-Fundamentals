c658d3cf015624139c2ca90024e18173a12bcae9
grantgabriel <grantgabriel30@gmail.com>
2023-11-01 08:04:57 +0700

    Finishes advanced and common data types

---
+++ Data Types/advanced.cpp
@@ -0,0 +1,62 @@
+// Filename : advanced.cpp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // int arrayOfIntegers[] = {1, 2, 3};      // Inisialisasi array int secara langsung
+    // string arrayOfStrings[2];               // Inisialisasi array string dengan alokasi memori
+
+    // arrayOfStrings[0] = "C++ Keren";        // Cara mengakses array menggunakan indeks, dimulai dari 0
+    // arrayOfStrings[1] = "Agah ini apa ya";  
+
+    // cout << arrayOfIntegers[2] << endl;     // 3
+    // cout << arrayOfStrings[0];              // C++ Keren
+    // cout << arrayOfIntegers[20];            // ERROR! Diluar batas array!
+
+    // vector<int> vectorInt = {1, 2 ,3};      // Inisialisasi nilai vector untuk indeks 0 1 2
+    // vectorInt.push_back(4);                 // Menambahkan angka 4 kebelakang vectorInt, di indeks 3.
+    // cout << vectorInt[3] << endl;           // 4
+    // vectorInt.erase(vectorInt.begin());     // Menghapus element pertama
+    // cout << vectorInt[0];                   // 2, bukan 1 lagi karena 1 telah dihapus.
+
+    // map<string, int> namaDanUmurMhs;           // Inisialisasi sebuah map dengan indeks string dan value int
+    // namaDanUmurMhs["Grant"] = 19;              // Untuk indeks "Grant", valuenya ialah 19
+    // namaDanUmurMhs["Alwin"] = 900;             // Untuk indeks "Alwin", valuenya ialah 900
+
+    // cout << namaDanUmurMhs["Alwin"] << endl;   // 900
+    // cout << namaDanUmurMhs["Nadya"];           // ERROR! Indeks belum ada didefenisikan
+
+    // stack<int> myStack;
+
+    // // Menambahkan elemen ke stack
+    // myStack.push(1);
+    // myStack.push(2);
+    // myStack.push(3);
+
+    // // Menghapus elemen dari stack, disini terhapus element 3
+    // myStack.pop();
+
+    // // Mengakses elemen paling atas stack
+    // cout << "Elemen paling atas: " << myStack.top() << endl; // 2
+
+    // // Menampilkan ukuran stack
+    // cout << "Ukuran stack: " << myStack.size() << endl;      // 2
+
+    // queue<int> myQueue;
+
+    // // Menambahkan elemen ke queue
+    // myQueue.push(1); myQueue.push(2);
+    // myQueue.push(3); myQueue.push(4);
+
+    // // Menghapus elemen pertama dari queue, yakni 1 
+    // myQueue.pop();
+
+    // // Mengakses elemen pertama dalam queue
+    // cout << "Elemen pertama: " << myQueue.front() << endl; // 2
+
+    // // Menampilkan ukuran queue
+    // cout << "Ukuran queue: " << myQueue.size() << endl;    // 3
+
+    return 0;
+}
---
+++ Data Types/common.cpp
@@ -0,0 +1,17 @@
+// Filename : common.cpp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int g = 5;
+    const int x = 100;  // Cara inisialisasi konstanta
+    float d = 2.5;
+    double e = 2.137;
+    char x = 'x';
+    string str = "C++ Bahasa Keren";
+
+    cout << str;  // Output : C++ Bahasa Keren 
+
+    return 0;
+}
