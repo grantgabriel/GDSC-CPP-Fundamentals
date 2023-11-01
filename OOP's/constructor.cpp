683470f847798844294de850c92c68e64d2d4894
grantgabriel <grantgabriel30@gmail.com>
2023-11-02 00:51:58 +0700

    Starting OOP and create README.md

---
+++ OOP's/constructor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+    private:
+        int speed;
+        string brands;
+
+    public:
+        Car() {
+            this->speed = 0;
+            this->brands = "No Brand";
+        }
+}
+
+int main() {
+
+    return 0;
+}
